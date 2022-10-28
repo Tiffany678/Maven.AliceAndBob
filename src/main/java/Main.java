de276a2763c09baefd4442a984e5805af3b48b1c
Yingjie <52tiffany.yang@gmail.com>
2022-10-27 22:17:13 -0400

    Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 /**
- * Created by iyasuwatts on 10/17/17.
+ * Created by Yingjie Yang on 10/27/17.
  */
 public class Main {
-
     public static void main(String[] args ){
+    //create Scanner object
+        Scanner stringName = new Scanner(System.in);
 
+        //prompt user for entering their name
+        System.out.print("Hi, please enter your name: ");
+        String name= stringName.nextLine();
+
+        //Greeting users whose name are Alice and Bob
+        if(name.equalsIgnoreCase("alice") || name.equalsIgnoreCase("bob")){
+            System.out.print("Hi, "+ name);
+        }
     }
 }
